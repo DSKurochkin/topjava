af29bf8a11cd8da5c5ffc448019d5c1f5cb1fd1c
Dmitriy Kurochkin <kurochkin.dm@gmail.com>
2021-03-22 14:04:50 +0300

    without optional/test no

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -1,16 +1,35 @@
 package ru.javawebinar.topjava.model;
 
-import javax.persistence.FetchType;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
+@NamedQueries({
+        @NamedQuery(name=Meal.GET, query = "SELECT m FROM Meal m WHERE m.id=:id AND m.user.id=?1"),
+        @NamedQuery(name=Meal.DELETE, query = "DELETE  FROM Meal m WHERE m.id=:id AND m.user.id=?1"),
+        @NamedQuery(name=Meal.ALL, query = "SELECT m FROM Meal m WHERE m.user.id=?1 ORDER BY m.dateTime DESC"),
+        @NamedQuery(name=Meal.ALL_FILTERED, query = "SELECT m FROM Meal m WHERE m.user.id=?1  " +
+                "AND m.dateTime >= ?2 AND m.dateTime < ?3 ORDER BY m.dateTime DESC")
+        
+})
+
+@Entity
+@Table(name="meals", uniqueConstraints
+        = {@UniqueConstraint(columnNames = {"user_id", "date_time"}, name= "meals_unique_user_datetime_idx" )})
 public class Meal extends AbstractBaseEntity {
+    
+    public static final String GET="Meal.get";
+    public static final String DELETE="Meal.del";
+    public static final String ALL="Meal.getAll";
+    public static final String ALL_FILTERED ="getBetweenHalfOpen";
+
+    @Column(name="date_time", nullable = false)
     private LocalDateTime dateTime;
 
+    @Column(name="description", nullable = false)
     private String description;
-
+    @Column(name="calories", nullable = false)
     private int calories;
 
     @ManyToOne(fetch = FetchType.LAZY)
